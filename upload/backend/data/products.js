9f2b73e82627ca1408f17e575ac0f4f8c5469ad4
HoangQuyla <66299668+HoangQuyla@users.noreply.github.com>
2021-03-31 23:33:27 +0930

    Add files via upload

---
+++ upload/backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'NT Bus Tracker ',
+    image: '/images/airpods.jpg',
+    description:
+      'A more visual interface. Real time location on screen Estimated time and the best route for your destination, Auto-mated text message service when your selected bus is nearby. A preferred route saving option',
+    brand: 'Apple',
+    category: 'People of all ages',
+    price: 89.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    name: 'Money Management',
+    image: '/images/phone.jpg',
+    description:
+    'This app will track income and expenses and provide relevant information',
+    brand: 'Apple',
+    category: 'working people between 18-40 years old',
+    price: 599.99,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 4,
+  },
+  {
+    name: 'Time management app',
+    image: '/images/camera.jpg',
+    description:
+    'Helps to manage schedule as the users can see graph of time division and it helps to know the live location of the user',
+    brand: 'Cannon',
+    category: 'Everyone',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    name: 'NT Fuel and Services',
+    image: '/images/playstation.jpg',
+    description:
+    'Store fuel purchase information, analyze your fuel expenses, find local services and navigate a fuel map',
+    brand: 'Sony',
+    category: 'Ages 18-40',
+    price: 399.99,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 3,
+  },
+  {
+    name: 'Pocket Library Alpha',
+    image: '/images/mouse.jpg',
+    description:
+    'Pocket Library demnstrates an internal structure of CDU library (Casuarina campus) as well as major functions adn indicators',
+    brand: 'Logitech',
+    category: 'Students and academics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 2,
+  },
+  {
+    name: 'Simple Memo Phone App',
+    image: '/images/alexa.jpg',
+    description:
+    'A web-based memo app designede for simplicity and functionality',
+    brand: 'Amazon',
+    category: 'Students and academics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 4,
+  },
+]
+
+export default products
---
+++ upload/frontend/src/screens/ProductScreen.js
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProductDetails } from '../actions/productActions'
+
+const ProductScreen = ({ match }) => {
+  const dispatch = useDispatch()
+
+  const productDetails = useSelector((state) => state.productDetails)
+  const { loading, error, product } = productDetails
+
+  useEffect(() => {
+    dispatch(listProductDetails(match.params.id))
+  }, [dispatch, match])
+
+  return (
+    <>
+      <Link className='btn btn-light my-3' to='/'>
+        Go Back
+      </Link>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating
+                  value={product.rating}
+                  text={`${product.rating} reviews`}
+                />
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Description: {product.description}
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={3}>
+          </Col>
+        </Row>
+      )}
+    </>
+  )
+}
+
+export default ProductScreen
